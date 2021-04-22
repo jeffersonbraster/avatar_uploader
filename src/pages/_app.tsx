019c87f13dac609b30707d2183b641e263e0e2d9
Jefferson <jeffersonbraster@gmail.com>
2021-04-22 15:10:19 -0300

    definindo estilos globais, criando a home.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,20 @@
 import type { AppProps /*, AppContext */ } from 'next/app'
 import Head from 'next/head'
 
-import GlobalStyles from 'styles/global'
+import '../styles/global.scss'
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>React Avançado - Boilerplate</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <title>Avatar upload</title>
+
         <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
-          content="A simple project startaer to work with Typescript, React, nextjs and styledComponents "
+          content="A simple project startaer to work with Typescript, React, nextjs, react-easy-crop "
         />
       </Head>
-      <GlobalStyles />
       <Component {...pageProps} />
     </>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,19 @@
+import { BsCardImage } from 'react-icons/bs'
+
+import styles from './home.module.scss'
+
 export default function Home() {
-  return <h1>hello</h1>
+  return (
+    <div className={styles.box}>
+      <div className={styles.boxContainer}>
+        <p className={styles.container}>
+          <span>
+            <BsCardImage title="Image Icon" />
+            <strong>Organization Logo</strong>
+          </span>
+          Drop the image here or click to browse.
+        </p>
+      </div>
+    </div>
+  )
 }
